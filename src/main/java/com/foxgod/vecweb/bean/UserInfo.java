63cde650c7627fcb1909ea0d92c531e2090b4216
BR6666 <934681102@qq.com>
2020-03-25 15:42:16 +0800

    制作Dashboard

---
+++ src/main/java/com/foxgod/vecweb/bean/UserInfo.java
@@ -0,0 +1,46 @@
+package com.foxgod.vecweb.bean;
+
+/**
+ * @Description
+ * @Author FoxGod
+ * @Date 2020/03/25 13:42
+ */
+public class UserInfo {
+
+    String name;
+    String num;
+    String createTime;
+    String lastTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(String lastTime) {
+        this.lastTime = lastTime;
+    }
+}
